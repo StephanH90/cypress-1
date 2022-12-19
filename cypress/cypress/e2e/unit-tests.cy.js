615e309ae22825a020045484c725fa9ada126a13
Stephan Hug <stephan.hug@adfinis.com>
2022-12-19 13:22:02 +0100

    basic setup for add function exercise

--- cypress/cypress/e2e/unit-tests.cy.js
+++ cypress/cypress/e2e/unit-tests.cy.js
@@ -1,6 +1,8 @@
 /// <reference types="Cypress" />
 
 describe("fizzbuzz function", () => {
+  // If you get the error that 'window.fizzbuzz' is
+  // undefined replace `before` with `beforeEach`
   beforeEach(() => {
     cy.visit('localhost')
   })
@@ -13,11 +15,41 @@ describe("fizzbuzz function", () => {
     })
   })
 
-  it('returns fizz if the number is divisible by 5', () => {
+  it('returns buzz if the number is divisible by 5', () => {
     cy.window().then((window) => {
       const result = window.fizzbuzz(5)
 
-      assert.equal(result, 'asdfasdf')
+      assert.equal(result, 'buzz')
     })
   })
+
+  it('returns fizzbuzz if the number is divisible by 3 and 5', () => {
+    cy.window().then((window) => {
+      const result = window.fizzbuzz(15)
+
+      assert.equal(result, 'fizzbuzz')
+    })
+  })
+
+  it('returns an error message if the number is not divisible by 3 or 5', () => {
+    cy.window().then((window) => {
+      const result = window.fizzbuzz(7)
+
+      assert.equal(result, 'Number must be divisible by 3 or 5')
+    })
+  })
+})
+
+describe("add function", () => {
+  beforeEach(() => {
+    cy.visit('localhost')
+  })
+  
+  it("should add the two supplied numbers", () => {
+    cy.window().then((window) => {
+      const result = window.add("2,3")
+
+      assert.equal(result, 5)
+    })
+  }) 
 })
--- webapp/index.php
+++ webapp/index.php
@@ -4,11 +4,27 @@
 <?php require "view/blocks/page_end.php"; ?>
 
 <script>
-class UserService {
-  validateUsername = function (username) {
-    // implement validation here
+window.fizzbuzz = function (number) {
+  let output = ''
+  const errorMsg = 'Number must be divisible by 3 or 5'
+  
+  if (number % 3 === 0) {
+    output += 'fizz'
   }
+  if (number % 5 === 0) {
+    output += 'buzz'
+  }
+
+  return output || errorMsg
 }
 
-window.UserService = UserService;
+window.add = function (numbersString) {
+  // "1,2,3".split(',')
+  // => ["1","2","3"]
+}
+
+// window.add("1,2")
+// => 3
+
+
 </script>
