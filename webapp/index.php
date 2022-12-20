5f5561365c1aea0df22f0e760dd1e82e1371af76
Stephan Hug <stephan.hug@adfinis.com>
2022-12-20 09:49:57 +0100

    make games with javascript

--- webapp/index.php
+++ webapp/index.php
@@ -1,14 +1,72 @@
 <?php $page_title = "Home ★ Productive"; ?>
 <?php require "view/blocks/page_start.php"; ?>
 <h1>Welcome to Productive!</h1>
+
+<div>
+  Time left: <span id="timer">30</span>
+</div>
+
+<div>
+  Score: <span id="score">0</span>
+</div>
+
+<button id="reset-btn">
+  Reset game
+</button>
+
+<div id="field" style="height: 400px; width: 400px; border: 1px solid red; position: absolute;">
+  <div
+    id="target"
+    style="height: 20px; width: 20px; background-color: red; position: relative;"
+  >
+  </div>
+</div>
 <?php require "view/blocks/page_end.php"; ?>
 
 <script>
-class UserService {
-  validateUsername = function (username) {
-    // implement validation here
+const decreaseTimer = function () {
+  const timer = document.querySelector('#timer')
+  const currentTime = parseInt(timer.innerText)
+  const newTime = currentTime - 1;
+  if (newTime <= 0) {
+    alert('game finished. score is: ' + document.querySelector('#score').innerText)
+    timer.innerText = 30;
+  } else {
+    timer.innerText = currentTime - 1;
   }
 }
 
-window.UserService = UserService;
+const jumpTarget = function () {
+  const target = document.querySelector('#target')
+  const newX = Math.floor(Math.random() * (400 - 50))
+  const newY = Math.floor(Math.random() * (400 - 50))
+  target.style.left = `${newX}px`
+  target.style.top = `${newY}px`
+}
+
+const increaseScore = function (scoreEl) {
+  const oldScore = parseInt(scoreEl.innerText)
+  scoreEl.innerText = oldScore + 1;
+}
+
+const target = document.querySelector('#target');
+const scoreEl = document.querySelector('#score');
+
+target.addEventListener('click', () => {
+  increaseScore(scoreEl);
+  jumpTarget();
+})
+
+document.querySelector('#reset-btn').addEventListener('click', () => {
+  document.querySelector('#score').innerText = 0;
+  document.querySelector('#timer').innerText = 30;
+})
+
+setInterval(() => {
+  decreaseTimer();
+}, 1000);
+
+setInterval(() => {
+  jumpTarget();
+}, 2000);
 </script>
